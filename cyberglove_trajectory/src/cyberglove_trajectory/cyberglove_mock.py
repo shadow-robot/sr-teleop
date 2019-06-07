f1510ccd74e1042c6b3d6c547fa391310c8d78e6
carebare47 <tom@shadowrobot.com>
2019-06-07 10:47:35 +0000

    tidying

--- cyberglove_trajectory/src/cyberglove_trajectory/cyberglove_mock.py
+++ cyberglove_trajectory/src/cyberglove_trajectory/cyberglove_mock.py
@@ -7,32 +7,13 @@ import rospy
 import rospkg
 import argparse
 import actionlib
-import tty
 import sys
-import tf
-import tf2_ros
-import yaml
-import geometry_msgs.msg
-import termios
-from std_srvs.srv import Trigger, TriggerRequest
-from threading import Thread, Lock
-from sr_vive_tracking.msg import ControllerEvent
 from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
-
-
-class VRCodes:
-    k_EButton_ApplicationMenu = 1
-    k_EButton_SteamVR_Trigger = 33
-    VREvent_ButtonPress = 200
-    VREvent_ButtonUnpress = 201
-    TrackedDeviceClass_Controller = 2
 
 class CybergloveMock(object):
     def __init__(self, cycle_hand_positions=True):
         self._cycle_hand_positions = cycle_hand_positions
-
-        self._button_publisher = rospy.Publisher('sr_vive_controller_events', ControllerEvent, queue_size=5)
         self._hand_traj_client = actionlib.SimpleActionClient('/sr_teleop_vive_cyberglove_trajectory_controller' +
                                                               '/follow_joint_trajectory',
                                                               FollowJointTrajectoryAction)
@@ -70,35 +51,13 @@ class CybergloveMock(object):
         goal.trajectory.points.append(joint_traj_point)
         self._hand_traj_client.send_goal(goal)
 
-    def _publish_button_event(self, device_id, button_id, event_id, device_type, device_index):
-        controller_event = ControllerEvent()
-        controller_event.header.stamp = rospy.Time.now()
-        controller_event.device_id = device_id
-        controller_event.button_id = button_id
-        controller_event.event_id = event_id
-        controller_event.device_type = device_type
-        controller_event.device_index = device_index
-        self._button_publisher.publish(controller_event)
-
-    def trigger_press(self):
-        self._publish_button_event(0, VRCodes.k_EButton_SteamVR_Trigger, VRCodes.VREvent_ButtonPress,
-                                   VRCodes.TrackedDeviceClass_Controller, 0)
-    def trigger_release(self):
-        self._publish_button_event(0, VRCodes.k_EButton_SteamVR_Trigger, VRCodes.VREvent_ButtonUnpress,
-                                   VRCodes.TrackedDeviceClass_Controller, 0)
-
     def run(self):
         if self._cycle_hand_positions:
             while not rospy.is_shutdown():
-                #self.trigger_press()
                 self.pack_hand()
-                rospy.loginfo("SPAM")
                 rospy.sleep(3)
                 self.open_hand()
-                rospy.loginfo("SPAM")
                 rospy.sleep(3)
-
-            #self.trigger_release()
 
 
 if __name__ == "__main__":
